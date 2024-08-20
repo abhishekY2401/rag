8c08475b0a5a349fc91f3d6c7f34aaef42b8832a
abhishekY2401 <abhiyd2401@gmail.com>
2024-08-21 00:09:57 +0530

    setup flask app factories

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from utils.chat import chat
-from utils.pdf import pdf
-
-app = Flask(__name__)
-
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
-CORS(app, resources={r"/*": {"origins": "*"}},
-     supports_credentials=True)  # Enable cors for all routes
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-app.register_blueprint(chat, url_prefix='/rag')
-app.register_blueprint(pdf, url_prefix='/rag')
+from rag.chat import chat
+from rag.pdf import pdf
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return "flask server running"
+def create_app():
+    app = Flask(__name__)
+
+    app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
+    CORS(app, resources={r"/*": {"origins": "*"}},
+         supports_credentials=True)  # Enable cors for all routes
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
+    app.register_blueprint(chat, url_prefix='/rag')
+    app.register_blueprint(pdf, url_prefix='/rag')
+
+    # Health check route
+    @app.route('/health', methods=['GET'])
+    def health_check():
+        return jsonify({"status": "Healthy"}), 200
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7000)
+    app = create_app()
+    app.run(port=7000)
