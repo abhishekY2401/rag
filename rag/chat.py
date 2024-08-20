4d323adea68ceee7fcfde0a26e5d205c09e1cadc
abhishekY2401 <abhiyd2401@gmail.com>
2024-08-21 00:11:46 +0530

    setup cohere query llm

---
+++ rag/chat.py
@@ -0,0 +1,48 @@
+from database.db import supabase
+from flask import Blueprint, request, jsonify
+from rag.agent.query import QueryAgent
+
+chat = Blueprint('chat', __name__)
+
+
+@chat.route('/chat/create', methods=['POST'])
+def createChat():
+    try:
+        data = request.get_json()
+        title = data.get('title')
+        description = data.get('description')
+
+        print(title, " ", description)
+
+        # create table and insert values
+        data, count = supabase.table('chat').insert({
+            "title": title,
+            "description": description
+        }).execute()
+
+        response = {
+            "msg": "succesfully created a chat",
+            "data": data[1][0]
+        }
+
+        return response
+    except Exception as e:
+        return jsonify({"msg": str(e)}), 500
+
+
+@chat.route('/chat', methods=['POST'])
+def chatWithLLM():
+    data = request.json
+    messages = data.get("messages", [])
+
+    if len(messages) == 1:
+        query = messages[0].get("content", '')
+        result = QueryAgent(
+            query=query,
+            num_chunks=30,
+            stream=False,
+        )
+        return jsonify(result)
+
+    else:
+        return jsonify({"error": "Multi-message input not implemented"}), 400
